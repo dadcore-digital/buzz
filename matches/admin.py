6b10abb1354ca5738bb6834df7869551a3b0d62c
ianfitzpatrick1 <ianfitz@gmail.com>
2020-12-21 17:21:53 -0600

    Minor change to how matches displayed in admin

--- matches/admin.py
+++ matches/admin.py
@@ -3,7 +3,8 @@ from .models import Match, Result, Set, Game
 
 class MatchAdmin(admin.ModelAdmin):
     
-    list_display = ('home', 'away', 'circuit', 'round', 'start_time', 'primary_caster')
+    list_display = ('id', 'home', 'away', 'circuit', 'round', 'start_time', 'primary_caster')
+    
     search_fields = (
         'home__name',
         'away__name',
