d3d2db63cb3325660d8fe0930bc6da26f40a63af
ianfitzpatrick1 <ianfitz@gmail.com>
2020-12-24 02:35:49 -0600

    Added more fields to stream model

--- streams/models.py
+++ streams/models.py
@@ -5,8 +5,13 @@ from casters.models import Caster
 class Stream(models.Model):
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    stream_id = models.CharField(max_length=255)
+    max_viewer_count = models.SmallIntegerField(blank=True, null=True)
+    thumbnail_url = models.CharField(max_length=255, blank=True, null=True)
 
     start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank=True, null=True)
     is_live = models.BooleanField(default=False)
 
     SERVICE_CHOICES = (
