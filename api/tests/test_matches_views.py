ea99501331f60b190e02cce7221725aa87bbe5a1
ianfitzpatrick1 <ianfitz@gmail.com>
2021-03-08 01:34:07 -0600

    Added tests for match API endpoints that bots rely on

--- api/tests/test_matches_views.py
+++ api/tests/test_matches_views.py
@@ -10,7 +10,7 @@ from api.tests.services import BuzzClient
 @mark.filterwarnings(
     'ignore:DateTimeField Match.start_time received a naive datetime')
 @mark.django_db
-def test_get_matches_next_24_hours(django_app):
+def test_get_matches_upcoming(django_app):
     """
     Get all upcmoing matches in next 24 hours
     """
@@ -50,4 +50,57 @@ def test_get_matches_next_24_hours(django_app):
     assert 'secondary_casters' in matches[0].keys()
     assert 'result' in matches[0].keys()
     assert 'vod_link' in matches[0].keys()
+
+
+@mark.django_db
+def test_get_team_matches(django_app):
+    """
+    Get all upcmoing matches in next 24 hours
+    """
+    client = BuzzClient(django_app)
+    circuit = CircuitFactory()
+
+    now = timezone.now()
+
+    match_1 = MatchFactory(circuit=circuit, start_time=now - timedelta(days=2))
+    team = match_1.home 
     
+    match_2 = MatchFactory(
+        circuit=circuit, away=team, start_time=now + timedelta(hours=2)) 
+    match_3 = MatchFactory(
+        circuit=circuit, home=team, start_time=now + timedelta(days=8))
+    match_4 = MatchFactory(
+        circuit=circuit, away=team, start_time=now + timedelta(days=17))
+
+    # These should not show up in results
+    MatchFactory(circuit=circuit, start_time=now + timedelta(hours=26))
+    MatchFactory(circuit=circuit, start_time=now - timedelta(hours=1))
+
+    params = f'team={team.name}&league={circuit.season.league.name}&season={circuit.season.name}'
+    resp = client.matches(params)    
+
+    assert resp['count'] == 4
+
+@mark.django_db
+def test_get_matches_starts_in_minutes(django_app):
+    """
+    Get matches starting in the next n minutes
+    """
+    client = BuzzClient(django_app)
+    circuit = CircuitFactory()
+
+    now = timezone.now()
+
+    match = MatchFactory(circuit=circuit, start_time=now + timedelta(minutes=5))
+    
+    MatchFactory(
+        circuit=circuit, start_time=now + timedelta(minutes=7)) 
+
+    MatchFactory(
+        circuit=circuit, start_time=now - timedelta(minutes=1))
+
+    params = 'starts_in_minutes=5'
+    resp = client.matches(params)    
+
+    assert resp['count'] == 1
+
