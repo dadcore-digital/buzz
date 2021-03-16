13295f2f549f8890d9fb7e1ccb9019822142a3be
ianfitzpatrick1 <ianfitz@gmail.com>
2021-03-16 18:48:10 -0500

    Made add-a-ton-of-matches dropdowns a bit easier to use

--- matches/forms.py
+++ matches/forms.py
@@ -9,7 +9,7 @@ class CircuitWidget(s2forms.ModelSelect2Widget):
     def build_attrs(self, base_attrs, extra_attrs=None):
         """Add select2 data attributes."""
         default_attrs = {"data-minimum-input-length": 0}
-        default_attrs["data-allow-clear"] = "false"
+        default_attrs["data-allow-clear"] = "true"
 
         default_attrs.update(base_attrs)
         attrs = super().build_attrs(default_attrs, extra_attrs=extra_attrs)
@@ -30,13 +30,44 @@ class CircuitWidget(s2forms.ModelSelect2Widget):
     ]
 
 class RoundWidget(s2forms.ModelSelect2Widget):
+
+    def build_attrs(self, base_attrs, extra_attrs=None):
+        """Add select2 data attributes."""
+        default_attrs = {"data-minimum-input-length": 0}
+        default_attrs["data-allow-clear"] = "true"
+
+        default_attrs.update(base_attrs)
+        attrs = super().build_attrs(default_attrs, extra_attrs=extra_attrs)
+
+        if "class" in attrs:
+            attrs["class"] += " django-select2"
+        else:
+            attrs["class"] = "django-select2"
+        return attrs
+
     search_fields = [
         'name__icontains',
         'season__name__icontains',
-        'round_number__iexact'
+        'season__league__name__icontains',
+        'round_number__iexact',
     ]
 
 class TeamWidget(s2forms.ModelSelect2Widget):
+
+    def build_attrs(self, base_attrs, extra_attrs=None):
+        """Add select2 data attributes."""
+        default_attrs = {"data-minimum-input-length": 0}
+        default_attrs["data-allow-clear"] = "true"
+
+        default_attrs.update(base_attrs)
+        attrs = super().build_attrs(default_attrs, extra_attrs=extra_attrs)
+
+        if "class" in attrs:
+            attrs["class"] += " django-select2"
+        else:
+            attrs["class"] = "django-select2"
+        return attrs
+
     search_fields = [
         "name__icontains",
     ]
@@ -59,15 +90,18 @@ class MatchForm(ModelForm):
 
         self.fields['circuit'].queryset = Circuit.objects.filter(
             season__is_active=True 
-        )
+        ).order_by('name')
+
         self.fields['round'].queryset = Round.objects.filter(
             season__is_active=True 
-        )
+        ).order_by('round_number')
+
         self.fields['home'].queryset = Team.objects.filter(
             circuit__season__is_active=True 
-        )        
+        ).order_by('name')
+
         self.fields['away'].queryset = Team.objects.filter(
             circuit__season__is_active=True 
-        )
+        ).order_by('name')
     
 MatchFormSet = formset_factory(MatchForm, extra=20)
