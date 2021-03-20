546819c3615c7f0589259afd958ab47c84d46f0c
ianfitzpatrick1 <ianfitz@gmail.com>
2021-03-19 22:38:32 -0500

    Added buzzsettings.github and sqlite db

---
+++ buzz/settings/github.py
@@ -0,0 +1,16 @@
+from .base import *  
+
+ALLOWED_HOSTS = ['buzz.local']
+DEBUG = True
+INTERNAL_IPS = ('127.0.0.1')
+
+STATIC_ROOT = '/var/www/buzz_static/'
+MEDIA_ROOT = '/var/www/buzz_media/'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
