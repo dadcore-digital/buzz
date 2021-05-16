deb7f0d4fe2ffd32fc5b405d108bbf332095a4ca
ianfitzpatrick1 <ianfitz@gmail.com>
2021-05-16 14:12:12 -0500

    Exposed award creation endpoint for authorized users, expanded api read access to stat backend

--- api/serializers/awards.py
+++ api/serializers/awards.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from awards.models import Award, AwardCategory
+from awards.models import Award, AwardCategory, Stat, StatCategory
+from awards.permissions import can_create_award
 
 class AwardCategorySerializer(serializers.ModelSerializer):
     
@@ -33,3 +34,60 @@ class AwardSerializer(serializers.ModelSerializer):
             'award_category', 'circuit', 'round', 'player', 'stats'
         ]
         depth = 1
+
+class CreateAwardStatSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Stat
+        fields = ['stat_category', 'total'] 
+
+class CreateAwardSerializer(serializers.ModelSerializer):
+    
+    award_category = serializers.IntegerField(source='award_category_id', style={'base_template': 'input.html', 'placeholder': 'Award Category ID'})
+    circuit = serializers.IntegerField(source='circuit_id', style={'base_template': 'input.html', 'placeholder': 'Circuit ID'})
+    round = serializers.IntegerField(source='round_id', style={'base_template': 'input.html', 'placeholder': 'Round ID'})
+    player = serializers.IntegerField(source='player_id', style={'base_template': 'input.html', 'placeholder': 'Player ID'})
+    stats = CreateAwardStatSerializer(many=True, read_only=False)
+
+    def create(self, validated_data):
+        stats_data = validated_data.pop('stats')
+        award = Award.objects.create(**validated_data)                                
+        
+        for data in stats_data:
+            stat = Stat.objects.create(**data)
+            award.stats.add(stat)
+        
+        return award
+
+    def validate(self, data):
+        """Only users in 'service' account group can create awards."""
+        user = self.context['request'].user
+
+        has_permission, error = can_create_award(user, return_error_msg=True)
+
+        if not has_permission:
+            raise serializers.ValidationError(error)
+
+        return data
+
+
+    class Meta:
+        model = Award
+        fields = [
+            'award_category', 'circuit', 'round', 'player', 'stats'
+        ]
+        depth = 1
+
+class StatCategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = StatCategory
+        fields = ['id', 'name']
+
+class StatSerializer(serializers.ModelSerializer):
+    
+    stat_category = StatCategorySerializer()
+
+    class Meta:
+        model = Stat
+        fields = ['id', 'stat_category', 'total']
